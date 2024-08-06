577de3c5adbc4773b14b1d7f2416159ff9e390fe
Alexander Nesterov <alex18nest@icloud.com>
2024-08-06 16:31:20 -0300

    [+] Preprocessor dirictive

--- Preprocessor directives/main.cpp
+++ Preprocessor directives/main.cpp
@@ -7,8 +7,32 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#define MODE 1
+#ifndef MODE
+#error "MODE is not defined. Please define MODE."
+#endif
+
+#if MODE == 1
+int add(int a, int b) {
+    return a + b;
+}
+#endif
+
+int main() {
+#if MODE == 0
+    std::cout << "Работаю в режиме тренировки" << std::endl;
+#elif MODE == 1
+    std::cout << "Работаю в боевом режиме" << std::endl;
+    
+    int x, y;
+    std::cout << "Введите число 1: " << std::endl;
+    std::cin >> x;
+    std::cout << "Введите число 2: " << std::endl;
+    std::cin >> y;
+    std::cout << "Результат сложения: " << add(x, y) << std::endl;
+#else
+    std::cout << "Неизвестный режим. Завершение работы" << std::endl;
+#endif
     return 0;
 }
+
